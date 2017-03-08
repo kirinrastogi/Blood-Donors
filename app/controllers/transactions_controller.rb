7df4b1c86510392eb404955d3fa454ced5b0b894
kirinrastogi <rastogikirin@gmail.com>
2017-03-08 13:48:51 -0500

    get all transactions of a donor in transactions route

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,6 +7,10 @@ class TransactionsController < ApplicationController
     render json: Transaction.find_each
   end
 
+  def donor
+    render json: Donor.find_by_sql("select * from donors join transactions on transactions.donor_id=donors.id where donors.id=#{params[:id]};")
+  end
+
   def create
   end
 end
