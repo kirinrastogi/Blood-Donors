1eace274cd5941c1af459252babfc80f9af72804
kirinrastogi <rastogikirin@gmail.com>
2017-03-09 20:28:14 -0500

    Update scripts to kill and generate

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,6 +4,6 @@ rails g model Recipient name email blood_type
 
 rails g model Nurse name email
 
-rails g model Transaction donor:references:index recipient:references:index
+rails g model Donation donor:references:index recipient:references:index
 
-rails g model PresentNurse transaction:references:index nurse:references:index
+rails g model PresentNurse donation:references:index nurse:references:index
