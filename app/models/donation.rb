4060fd46ddd2ef4925d6ff34496f97877a721c43
kirinrastogi <rastogikirin@gmail.com>
2017-03-10 00:55:40 -0500

    associations for nurse and donation

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,6 @@
 class Donation < ApplicationRecord
   belongs_to :donor
   belongs_to :recipient
+  belongs_to :present_nurse
+  has_many :nurses, :through => :present_nurses
 end
--- app/models/nurse.rb
+++ app/models/nurse.rb
@@ -1,2 +1,4 @@
 class Nurse < ApplicationRecord
+  has_many :present_nurses
+  has_many :donations, :through => :present_nurses
 end
