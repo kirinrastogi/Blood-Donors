e50e3bf18f9c075071047fbcb70c1f24febe10c5
kirinrastogi <rastogikirin@gmail.com>
2017-03-12 12:56:35 +0000

    change routes to be get for persons. abstract code for testing response

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,30 +1,18 @@
 class DonationsController < ApplicationController
   def show
-    @donation = Donor.joins(:donations => :recipient)
-      .where('donations.id = ?', params[:id])
-      .select(
-        'donations.created_at', 
-        'donations.id',
-        'donations.recipient_id',
-        'donations.donor_id',
-        'recipients.name as recipient_name',
-        'recipients.email as recipient_email',
-        'donors.name as donor_name',
-        'donors.email as donor_email'
-    ).first
+    @donation = show_json
   end
-
 
   def index
     render json: Donation.find_each
   end
 
   def donor
-    render json: Donation.includes(:donor).where(donor_id: params[:id])
+    render json: donor_json
   end
 
   def recipient
-    render json: Donation.includes(:recipient).where(recipient_id: params[:id])
+    render json: recipient_json
   end
 
   def new
@@ -32,7 +20,7 @@ class DonationsController < ApplicationController
   end
 
   def create
-    @donation = Donation.new donor_params
+    @donation = Donation.new donation_params
     Donation.validate!(params)
     @donation.save!
     redirect_to @donation
@@ -43,6 +31,32 @@ class DonationsController < ApplicationController
   private
 
   def donation_params
-    params.require(:donation)
+    params.require(:donation).permit(:recipient_id, :donor_id)
   end
+
+  def show_json
+    Donor.joins(:donations => :recipient)
+      .where('donations.id = ?', params[:id])
+      .select(
+    'donations.created_at', 
+    'donations.id',
+    'donations.recipient_id',
+    'donations.donor_id',
+    'recipients.name as recipient_name',
+    'recipients.email as recipient_email',
+    'donors.name as donor_name',
+    'donors.email as donor_email'
+    ).first
+
+  end
+
+  def donor_json
+    Donation.includes(:donor).where(donor_id: params[:id])
+  end
+
+  def recipient_json
+    Donation.includes(:recipient).where(recipient_id: params[:id])
+  end
+
+  helper_method(:show_json, :donor_json, :recipient_json)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :donors, only: [:show, :index, :create, :new]
   post '/recipients/affected' => 'recipients#affected'
   resources :donations, only: [:show, :index, :create, :new]
-  post '/donations/donor' => 'donations#donor'
-  post '/donations/recipient' => 'donations#recipient'
+  get '/donations/donor/:id' => 'donations#donor'
+  get '/donations/recipient/:id' => 'donations#recipient'
   resources :recipients, only: [:show, :index, :create]
 end
