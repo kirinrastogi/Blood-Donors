317280ed53a896ba438ff5efc54c1104f177dffd
kirinrastogi <rastogikirin@gmail.com>
2017-03-15 17:01:36 -0400

    Add validations for recipients

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,4 +1,7 @@
 class Recipient < ApplicationRecord
   has_many :donations
   has_many :donors, :through => :donations
+  validates :email, presence: true, format: { with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i }
+  validates :name, presence: true
+  validates :blood_type, presence: true, format: { with: /\A(a|b|ab|o)[+-]\z/i }
 end
