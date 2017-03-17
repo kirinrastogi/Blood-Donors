31a20763693ff733461977443a213c6a27a01421
kirinrastogi <rastogikirin@gmail.com>
2017-03-17 16:16:58 -0400

    tests for recipients affected without dates

--- test/controllers/recipients_controller_test.rb
+++ test/controllers/recipients_controller_test.rb
@@ -33,16 +33,50 @@ class RecipientsControllerTest < ActionDispatch::IntegrationTest
 
   test 'get show response format' do
     get recipients_url + '2/'
-    recipient = @controller.send(:show_json)
+    recipient = @controller.send :show_json
     assert_kind_of Recipient, recipient
   end
 
   test 'get show response properties' do
     get recipients_url + '2/'
-    recipient = @controller.send(:show_json)
+    recipient = @controller.send :show_json
     assert_equal 'recName2', recipient.name
     assert_equal 2, recipient.id
     assert_equal 'recEmail2@email.com', recipient.email
     assert Recipient.new(recipient.attributes).validate
   end
+
+  test 'get affected response' do
+    get recipients_url + 'affected/1'
+    assert_response :ok
+  end
+
+  test 'get affected response format' do
+    get recipients_url + 'affected/1'
+    assert_response :ok
+    assert_equal 'text/html', @response.content_type
+    assert_kind_of String, @response.body
+  end
+
+  test 'should have a recipient list of right format' do
+    get recipients_url + 'affected/1'
+    recipients = @controller.send :affected_json
+    assert_not recipients.empty?
+    assert_equal 2, recipients.length
+    assert_kind_of Donor::ActiveRecord_Relation, recipients
+    assert_kind_of Donor, recipients[0]
+  end
+
+  test 'should have recipient list of length 1' do
+    get recipients_url + 'affected/2'
+    recipients = @controller.send :affected_json
+    assert_equal 1, recipients.length
+  end
+
+  test 'should have empty recipient list' do
+    get recipients_url + 'affected/3'
+    recipients = @controller.send :affected_json
+    assert recipients.empty?
+  end
+
 end
