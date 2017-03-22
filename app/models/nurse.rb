66ba567a4c6851a41662b21932ade4978a205ff5
kirinrastogi <rastogikirin@gmail.com>
2017-03-22 22:09:56 +0000

    Add validations for nurse

--- app/models/nurse.rb
+++ app/models/nurse.rb
@@ -1,4 +1,6 @@
 class Nurse < ApplicationRecord
   has_many :present_nurses
   has_many :donations, :through => :present_nurses
+  validates :email, presence: true, format: { with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i }
+  validates :name, presence: true
 end
