cb07c0e399469716a4a50165e6c225aa9f17f41b
kirinrastogi <rastogikirin@gmail.com>
2017-03-22 22:10:08 +0000

    populate db with nurses and present nurses

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,16 +19,23 @@ def getBloodType
   end
 end
 
-SIZE = 1000
-DENSITY = 3
+SIZE = 100
+DONATION_SIZE = SIZE * 3
+NURSE_SIZE = SIZE / 20
 
-puts 'Creating donors and recipients'
+puts 'Creating donors and recipients and nurses'
 SIZE.times do |id|
   Donor.create(id: id, name: "donName#{id}", email: "don#{id}@email.com", blood_type: getBloodType)
   Recipient.create(id: id, name: "recName#{id}", email: "rec#{id}@email.com", blood_type: getBloodType)
+  if id < NURSE_SIZE
+    Nurse.create(id: id, name: "nurse#{id}", email: "nurseEmail#{id}@email.com")
+  end
 end
 
-puts "Creating donations"
-(SIZE * DENSITY).times do |id|
+puts "Creating donations and present_nurses"
+DONATION_SIZE.times do |id|
   Donation.create(id: id, donor_id: rand(SIZE), recipient_id: rand(SIZE))
+  if id < NURSE_SIZE
+    PresentNurse.create(id: id, donation_id: rand(DONATION_SIZE), nurse_id: rand(NURSE_SIZE))
+  end
 end
