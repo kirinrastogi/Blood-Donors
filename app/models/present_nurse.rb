02089f73e481181d1703ffde93e6ad9e10826543
kirinrastogi <rastogikirin@gmail.com>
2017-03-22 22:12:30 +0000

    validations for present_nurse

--- app/models/present_nurse.rb
+++ app/models/present_nurse.rb
@@ -1,4 +1,11 @@
 class PresentNurse < ApplicationRecord
   belongs_to :donation
   belongs_to :nurse
+  validates :donation_id, :nurse_id, presence: true
+  validate :validate_ids
+
+  def validate_ids
+    errors.add(:nurse_id, 'is invalid') unless Nurse.exists?(nurse_id)
+    errors.add(:donation_id, 'is invalid') unless Donation.exists?(donation_id)
+  end
 end
