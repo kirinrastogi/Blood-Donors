6728297fb7a83100fc0f83459e61553858474072
kirinrastogi <rastogikirin@gmail.com>
2017-03-23 11:46:36 -0400

    add nurse model tests

--- test/models/nurse_test.rb
+++ test/models/nurse_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class NurseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save with no params' do
+    nurse = Nurse.new
+    assert_not nurse.save
+  end
+
+  test 'should not save with just email' do
+    nurse = Nurse.new email: 'email@email.com'
+    assert_not nurse.save
+  end
+
+  test 'should not save with just name' do
+    nurse = Nurse.new name: 'nurseName'
+    assert_not nurse.save
+  end
+
+  test 'should save with only name and email provided' do
+    nurse = Nurse.new(name: 'nurseName', email: 'email@email.com')
+    assert nurse.save
+  end
+
+  test 'should not save with invalid email should fail' do
+    nurse = Nurse.new(name: 'nurseName', email: 'email')
+    assert_not nurse.save
+  end
 end
